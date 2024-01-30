ad3cf86ab5ffc164b7591563a5e2f0f29cb49e13
IsaacKimo <139249129+IsaacKimo@users.noreply.github.com>
2024-01-30 11:01:39 +0000

    commit

--- webapp/src/api-actions/alertAction.js
+++ webapp/src/api-actions/alertAction.js
@@ -1,5 +1,5 @@
 import { removeAlert, setAlert } from "@/redux/slices/alertSlice";
-import { v4 as uuidv4 } from "uuid";
+const { v4: uuidv4 } = require('uuid');
 
 export const set_Alert =
   (msg, alertType, timeout = 3000) =>
